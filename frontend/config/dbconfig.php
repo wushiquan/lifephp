b33454bea31e70f23da4c3fd389bab6f77221ec9
吴世权 <13824394613@163.com>
2016-11-15 21:52:44 +0800

    new version commit by shiquan.wu

---
+++ frontend/config/dbconfig.php
@@ -0,0 +1,20 @@
+<?php
+// -----------------------------------------------------------------------
+// + LifePHP - Make coding life more efficient,Let the program run faster
+// -----------------------------------------------------------------------
+// + LifePHP database configuration array information
+// -----------------------------------------------------------------------
+// + Copyright (c) 2016 http://www.lifephp.net  All rights reserved
+// -----------------------------------------------------------------------
+// + Author:  shiquanwu <2448154608@qq.com>
+// -----------------------------------------------------------------------
+// + Version: 1.0
+// -----------------------------------------------------------------------
+
+return [
+	'class' => 'lifephp\database\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=test',
+    'username' => 'root',
+    'password' => '123456',
+    'charset' => 'utf8',
+];
