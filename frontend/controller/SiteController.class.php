8ba3e6e9e43506174745b5a73b53d9fbf813ebbd
吴世权 <13824394613@163.com>
2016-11-15 21:47:38 +0800

    new version

--- frontend/controller/SiteController.class.php
+++ frontend/controller/SiteController.class.php
@@ -2,7 +2,7 @@
 // -----------------------------------------------------------------------
 // + LifePHP - Make coding life more efficient,Let the program run faster
 // -----------------------------------------------------------------------
-// + LifePHP core class
+// + Application site controller
 // -----------------------------------------------------------------------
 // + Copyright (c) 2016 http://www.lifephp.net  All rights reserved
 // -----------------------------------------------------------------------
@@ -11,11 +11,13 @@
 // + Version: 1.0
 // -----------------------------------------------------------------------
 
-namespace ubox\controller;
+namespace frontend\controller;
 
+use Life;
 use lifephp\mvc\Controller;
 use lifephp\common\Tool;
-
+use lifephp\database\Connection;
+use frontend\models\Comment;
 class SiteController extends Controller
 {
     /**
@@ -25,6 +27,36 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
+    	//INSRET EXAMPLE
+        $CommnetModel = new Comment();
+        $CommnetModel->user_id = mt_rand(1,1000000);
+        $CommnetModel->content = "love you " . $CommnetModel->user_id;
+        $rows = $CommnetModel->insert();
+
+        //SELECT EXAMPLE
+        //$record = Comment::model()->queryAll(['select'=>'id,content','where'=>'user_id > 1000']);        
+        //$record = Comment::model()->queryOne(['select'=>'id,content','where'=>'user_id = 2']);   
+
+        // $connection = Life::$frame->db;
+        // $userid = mt_rand(1,1000000);
+        // $sql = 'INSERT INTO `tbl_comment` (content,user_id) values("love you'.$userid.'","'.$userid.'")';
+        // $rows = $connection->createCommand($sql)->execute();
+
+        // $connection = Life::$frame->db;
+        // $sql = 'SELECT * FROM `tbl_comment`';
+        // $rows = $connection->createCommand($sql)->queryAll();  
+
+        Tool::printr($rows);
     	$this->display('index',['order'=>'SM23748934','username'=>'13824394613']);
+    }
+
+    /**
+     * @uses   The test action which is used for test. 
+     * @access public
+     * @return void
+     */
+    public function actionTest()
+    {
+        $this->redirect('/index.php?r=test/index&test=action&name=wushiquan',2,'redirect after 2s');    
     }
 }
