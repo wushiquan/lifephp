185013387a1b9e3e5fd88d6e5580de1b78404d2f
吴世权 <13824394613@163.com>
2017-01-01 13:34:24 +0800

    Update Tool.class.php

--- lifephp/common/Tool.class.php
+++ lifephp/common/Tool.class.php
@@ -36,4 +36,53 @@ class Tool
  		}
     }
 
+    /**
+     * @uses  XML encoding
+     * @param mixed $data data
+     *        string $root root node name
+     *        string $item sub node name with numbers as index
+     *        string $attr root node attribute
+     *        string $id   the attribute name changed by the key of sub node name with numbers as index
+     *        string $encoding the encoded data 
+     * @return string
+     */
+    public static function xml_encode($data, $root = 'lifephp', $item = 'item', $attr = '', $id = 'id', $encoding = 'utf-8')
+    {
+        if (is_array($attr)) {
+            $_attr = array();
+            foreach ($attr as $key => $value) {
+                $_attr[] = "{$key}=\"{$value}\"";
+            }
+            $attr = implode(' ', $_attr);
+        }
+        $attr = trim($attr);
+        $attr = empty($attr) ? '' : " {$attr}";
+        $xml  = "<?xml version=\"1.0\" encoding=\"{$encoding}\"?>";
+        $xml .= "<{$root}{$attr}>";
+        $xml .= self::data_to_xml($data, $item, $id);
+        $xml .= "</{$root}>";
+        return $xml;
+    }
+
+    /**
+     * @uses  data XML encoding
+     * @param mixed  $data data
+     * @param string $item sub node name with numbers as index
+     * @param string $id   the attribute name changed by the key of sub node name with numbers as index
+     * @return string
+     */
+    private static function data_to_xml($data, $item = 'item', $id = 'id')
+    {
+        $xml = $attr = '';
+        foreach ($data as $key => $val) {
+            if (is_numeric($key)) {
+                $id && $attr = " {$id}=\"{$key}\"";
+                $key         = $item;
+            }
+            $xml .= "<{$key}{$attr}>";
+            $xml .= (is_array($val) || is_object($val)) ? self::data_to_xml($val, $item, $id) : $val;
+            $xml .= "</{$key}>";
+        }
+        return $xml;
+    }
 }
